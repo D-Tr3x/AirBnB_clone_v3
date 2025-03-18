fb52e56988453763ec5c553557e1bac15545990e
D-Tr3x <owosenidaniel13@gmail.com>
2025-03-18 13:02:58 +0100

    Fixed docstring in module's documentation

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Implements all default RESTful API actions for State."""
+"""Implements all default RESTful API actions for City objects."""
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from models import storage
